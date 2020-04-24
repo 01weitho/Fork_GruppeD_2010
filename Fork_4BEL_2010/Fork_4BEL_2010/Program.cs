186aaf36b44d41fed10a42961e0ce716f58ac91b
02zoetho <62854195+02zoetho@users.noreply.github.com>
2020-04-24 11:42:34 +0200

    dezimalmethode

--- Fork_4BEL_2010/Fork_4BEL_2010/Program.cs
+++ Fork_4BEL_2010/Fork_4BEL_2010/Program.cs
@@ -100,6 +100,41 @@ namespace Fork_4BEL_2010
 
             return Hex;
         }
+
+        static string Dezimalmethode(string a)
+        {
+
+            string bin = a;
+            String Dezimalwert;
+            int Dezimalint = 0;
+            int bo = 0;
+
+
+            byte[] bitearray = Encoding.ASCII.GetBytes(bin);
+
+            for (int i = 0; i < bitearray.Length; i++)
+            {
+                if (bitearray[i] == 48)
+                {
+
+                    bitearray[i] = 0;
+                }
+                else
+                {
+                    bitearray[i] = 1;
+                }
+            }
+
+            for (int i = bitearray.Length - 1; i > -1; i--)
+            {
+                Dezimalint += bitearray[i] * Convert.ToInt32(Math.Pow(2, bo));
+                bo++;
+            }
+
+            Dezimalwert = Convert.ToString(Dezimalint);
+
+            return Dezimalwert;
+        }
       
         static string start()
         { 
